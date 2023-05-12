ad0056ab418f3bb6824e6c5177d0a9974e2be2a7
ruspat1 <ruspat1@us.es>
2023-05-12 12:00:15 +0200

    tercera

--- environment-server/services/environment_statsService.js
+++ environment-server/services/environment_statsService.js
@@ -1,12 +1,16 @@
 export function getEnvironment_stats(req, res) {
     res.send({
-        message: 'This is the mockup controller for getEnvironment_stats'
+        year: "2023",
+        city: "Sevilla"
     });
 }
 
 export function findByyear(req, res) {
     res.send({
-        message: 'This is the mockup controller for findByyear'
+
+        year: "2023",
+        city: "Sevilla"
+        
     });
 }
 
